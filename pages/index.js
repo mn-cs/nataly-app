3348a95cead4495d2e53da59f65ff8bc9929ac3e
mn-cs <pash.hayk@gmail.com>
2020-10-08 15:48:15 -0700

    adding head

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import App from './[App]/App.jsx'
 // import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div >
+        <Head>
+          <title>Natalie's blog</title>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <App/>
     </div>
   )
