de7edf7b3cc0fbf00d43b2d736b947796274c0e6
mn-cs <pash.hayk@gmail.com>
2020-10-27 17:04:06 -0700

    enabeling x-power

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,10 @@ import Footer from '../Components/Footer';
 import Nav from '../Components/Nav';
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
   return (
+    
       <>
       <Header/>
       <Nav/>
